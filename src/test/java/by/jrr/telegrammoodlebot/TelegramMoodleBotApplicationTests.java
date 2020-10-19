209a5e305663c58c6960ff579217c52c4cc5f78b
max-java <6666350@gmail.com>
2020-10-19 03:12:06 +0300

    initial commit bot

---
+++ src/test/java/by/jrr/telegrammoodlebot/TelegramMoodleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.telegrammoodlebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramMoodleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
